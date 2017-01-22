2cd4eccfaf4194a6bc3fff882217dffc72e97803
Yeva Yu <yevayu@hotmail.com>
2017-01-21 20:07:00 -0500

    Added a config file

--- index.js
+++ index.js
@@ -1,72 +1,75 @@
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 var url = 'mongodb://yevayu:jYuv2nTQvSeAV38EduMZ8bkAdGKSHjaugRPPZAQ9MUDJPJ1QsOpXXkhGmkaY8Z8nqFW2diQTDO1BFtpr98aAIQ==@yevayu.documents.azure.com:10250/?ssl=true'
-
-
 var fs = require('fs');
-var doc = fs.readFileSync('./renamed.csv', 'utf8');
+var config = require('./config.json');
 
+var filesToConvert = config.filesToConvert;
 var arr = [];
-var item = {};
-var splitted = doc.split('');
-var counter = 0;
-var quoteOpen = false;
-var bracketOpen = false;
-var tempArr = [];
-var str = '';
 
-for (var i = 0; i < splitted.length; ++i) {
-  var c = splitted[i];
-  if ( c === ',' && !quoteOpen && !bracketOpen ) {
-    if (counter%3 == 0) {
-      str = str.trim();
-      item.offense = str;
+for (fileName in filesToConvert) {
+  var name = './' + filesToConvert[fileName];
+  var doc = fs.readFileSync(name, 'utf8');
+
+  var item = {};
+  var splitted = doc.split('');
+  var counter = 0;
+  var quoteOpen = false;
+  var bracketOpen = false;
+  var tempArr = [];
+  var str = '';
+
+  for (var i = 0; i < splitted.length; ++i) {
+    var c = splitted[i];
+    if ( c === ',' && !quoteOpen && !bracketOpen ) {
+      if (counter%3 == 0) {
+        str = str.trim();
+        item.offense = str;
+        str = '';
+        counter++;
+      } else if (counter%3 == 1) {
+        str = str.trim();
+        item.penalty = str;
+        str = '';
+        counter++;
+      }
+      continue;
+    } else if ( c === '\"' && quoteOpen ) {
+      quoteOpen = false;
+      continue;
+    } else if ( c === '\"' && !quoteOpen ) {
+      quoteOpen = true;
+      continue;
+    } else if ( c === '|' && bracketOpen ) {
+      str = str.toLowerCase().trim();
+      tempArr.push(str);
       str = '';
-      counter++;
-    } else if (counter%3 == 1) {
-      console.log("penalll: " + str);
-      str = str.trim();
-      item.penalty = str;
+      continue;
+    } else if ( c === '\"' && !quoteOpen) {
+      quoteOpen = true;
+      continue;
+    } else if ( c === '\"' && quoteOpen) {
+      quoteOpen = false;
+      continue;
+    } else if ( c === '[' ) {
+      bracketOpen = true;
+      continue;
+    } else if ( c === ']' ) {
+      str = str.toLowerCase().trim();
+      tempArr.push(str);
       str = '';
+      item.category = tempArr;
+      tempArr = [];
+      arr.push(item);
+      item = {};
       counter++;
+      bracketOpen = false;
+      continue;
     }
-    continue;
-  } else if ( c === '\"' && quoteOpen ) {
-    quoteOpen = false;
-    continue;
-  } else if ( c === '\"' && !quoteOpen ) {
-    quoteOpen = true;
-    continue;
-  } else if ( c === '|' && bracketOpen ) {
-    tempArr.push(str);
-    str = '';
-    continue;
-  } else if ( c === '\"' && !quoteOpen) {
-    quoteOpen = true;
-    continue;
-  } else if ( c === '\"' && quoteOpen) {
-    quoteOpen = false;
-    continue;
-  } else if ( c === '[' ) {
-    bracketOpen = true;
-    continue;
-  } else if ( c === ']' ) {
-    str = str.trim();
-    tempArr.push(str);
-    str = '';
-    item.category = tempArr;
-    tempArr = [];
-    arr.push(item);
-    item = {};
-    counter++;
-    bracketOpen = false;
-    continue;
+    str += c;
   }
-  str += c;
 }
 
 MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
   console.log("Success!");
   insert(db, function(err, data) {
     db.close();
